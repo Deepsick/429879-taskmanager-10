a2cdafde37623937fbe3257b71c31053554a6ff2
deepsick <mav96@bk.ru>
2019-12-09 16:08:07 +0300

    Добавляет контроллер и абстрактный компонент

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -1,5 +1,6 @@
-import {COLORS, DAYS, MONTH_NAMES} from '../const.js';
-import {createElement, formatTime} from '../utils.js';
+import AbstractComponent from './abstract-component';
+import {COLORS, DAYS, MONTH_NAMES} from '../const';
+import {formatTime} from '../utils/common';
 
 const createColorsMarkup = (colors, currentColor) => {
   return colors
@@ -179,25 +180,20 @@ const createTaskEditTemplate = (task) => {
   );
 };
 
-export default class TaskEdit {
+export default class TaskEdit extends AbstractComponent {
   constructor(task) {
+    super();
     this._task = task;
-    this._element = null;
   }
 
   getTemplate() {
     return createTaskEditTemplate(this._task);
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
+  setSubmitHandler(handler) {
+    this
+      .getElement()
+      .querySelector(`form`)
+      .addEventListener(`submit`, handler);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,98 +1,26 @@
-import BoardComponent from './components/board';
-import SortComponent from './components/sort';
-import FilterComponent from './components/filter';
-import LoadMoreButtonComponent from './components/load-more-button';
-import TaskEditComponent from './components/task-edit';
-import TaskComponent from './components/task';
-import TasksComponent from './components/tasks';
-import NoTasksComponent from './components/no-tasks';
-import SiteMenuComponent from './components/site-menu';
-import {generateTasks} from './mock/task';
-import {generateFilters} from './mock/filter';
-import {render, RenderPosition} from './utils';
+import BoardComponent from './components/board.js';
+import BoardController from './controllers/board.js';
+import FilterComponent from './components/filter.js';
+import SiteMenuComponent from './components/site-menu.js';
+import {generateTasks} from './mock/task.js';
+import {generateFilters} from './mock/filter.js';
+import {render, RenderPosition} from './utils/render.js';
 
 const TASK_COUNT = 22;
-const SHOWING_TASKS_COUNT_ON_START = 8;
-const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
-
-const ESCAPE_NAMES = [`Escape`, `Esc`];
-
-const renderTask = (task, container) => {
-
-  const replaceEditToTask = () => {
-    container.replaceChild(taskComponent.getElement(), taskEditComponent.getElement());
-  };
-
-  const replaceTaskToEdit = () => {
-    container.replaceChild(taskEditComponent.getElement(), taskComponent.getElement());
-  };
-
-  const onEscKeyDown = (evt) => {
-    const isEscKey = ESCAPE_NAMES.includes(evt.key);
-
-    if (isEscKey) {
-      replaceEditToTask();
-      document.removeEventListener(`keydown`, onEscKeyDown);
-    }
-  };
-
-  const taskComponent = new TaskComponent(task);
-  const editButton = taskComponent.getElement().querySelector(`.card__btn--edit`);
-  editButton.addEventListener(`click`, () => {
-    replaceTaskToEdit();
-    document.addEventListener(`keydown`, onEscKeyDown);
-  });
-
-  const taskEditComponent = new TaskEditComponent(task);
-  const editForm = taskEditComponent.getElement().querySelector(`form`);
-  editForm.addEventListener(`submit`, replaceEditToTask);
-
-  render(container, taskComponent.getElement(), RenderPosition.BEFOREEND);
-};
 
 const siteMainElement = document.querySelector(`.main`);
 const siteHeaderElement = siteMainElement.querySelector(`.main__control`);
 
-render(siteHeaderElement, new SiteMenuComponent().getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, new SiteMenuComponent(), RenderPosition.BEFOREEND);
 
 const filters = generateFilters();
-render(siteMainElement, new FilterComponent(filters).getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterComponent(filters), RenderPosition.BEFOREEND);
 
 const boardComponent = new BoardComponent();
-render(siteMainElement, boardComponent.getElement(), RenderPosition.BEFOREEND);
+render(siteMainElement, boardComponent, RenderPosition.BEFOREEND);
 
 const tasks = generateTasks(TASK_COUNT);
-const isAllTasksArchived = tasks.every((task) => task.isArchive);
 
-if (isAllTasksArchived) {
-  render(boardComponent.getElement(), new NoTasksComponent().getElement(), RenderPosition.BEFOREEND);
-} else {
-  render(boardComponent.getElement(), new SortComponent().getElement(), RenderPosition.BEFOREEND);
-  render(boardComponent.getElement(), new TasksComponent().getElement(), RenderPosition.BEFOREEND);
+const boardController = new BoardController(boardComponent);
 
-  const taskListElement = siteMainElement.querySelector(`.board__tasks`);
-
-  let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
-  tasks
-    .slice(0, showingTasksCount)
-    .forEach((task) => renderTask(task, taskListElement));
-
-  const loadMoreButtonComponent = new LoadMoreButtonComponent();
-  render(boardComponent.getElement(), loadMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
-
-
-  loadMoreButtonComponent.getElement().addEventListener(`click`, () => {
-    const prevTasksCount = showingTasksCount;
-    showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
-
-    tasks.slice(prevTasksCount, showingTasksCount)
-      .forEach((task) => renderTask(task, taskListElement));
-
-    if (showingTasksCount >= tasks.length) {
-      loadMoreButtonComponent.getElement().remove();
-      loadMoreButtonComponent.removeElement();
-    }
-  });
-}
-
-
+boardController.render(tasks);
