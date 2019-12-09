38a7dc89cf2afee924476c4a483cfb48c9334b9d
deepsick <mav96@bk.ru>
2019-12-09 20:06:53 +0300

    Добавляет сортировку

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,16 +1,49 @@
 import AbstractComponent from './abstract-component';
 
+export const SortType = {
+  DATE_DOWN: `date-down`,
+  DATE_UP: `date-up`,
+  DEFAULT: `default`,
+};
+
+const LINK_TAG_NAME = `a`;
+
 const createSortTemplate = () => (
   `<div class="board__filter-list">
-    <a href="#" class="board__filter">SORT BY DEFAULT</a>
-    <a href="#" class="board__filter">SORT BY DATE up</a>
-    <a href="#" class="board__filter">SORT BY DATE down</a>
+    <a href="#" data-sort-type="${SortType.DEFAULT}" class="board__filter">SORT BY DEFAULT</a>
+    <a href="#" data-sort-type="${SortType.DATE_UP}" class="board__filter">SORT BY DATE up</a>
+    <a href="#" data-sort-type="${SortType.DATE_DOWN}" class="board__filter">SORT BY DATE down</a>
   </div>
   <div class="board__tasks"></div>`
 );
 
 export default class Sort extends AbstractComponent {
+  constructor() {
+    super();
+    this._currentSortType = SortType.DEFAULT;
+  }
+
   getTemplate() {
     return createSortTemplate();
+  }
+
+  setSortTypeChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName.toLowerCase() !== LINK_TAG_NAME) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currenSortType === sortType) {
+        return;
+      }
+
+      this._currenSortType = sortType;
+
+      handler(this._currenSortType);
+    });
   }
 }
